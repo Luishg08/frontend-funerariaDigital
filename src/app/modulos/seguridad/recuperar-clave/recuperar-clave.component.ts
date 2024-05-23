243aa56f1a27c4a11adf1316bd5ba195970b4891
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-05-23 17:39:43 -0500

    Se creó la validacion de recuperar clave

--- src/app/modulos/seguridad/recuperar-clave/recuperar-clave.component.ts
+++ src/app/modulos/seguridad/recuperar-clave/recuperar-clave.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-clave',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './recuperar-clave.component.css'
 })
 export class RecuperarClaveComponent {
+  fbGroup: FormGroup=new FormGroup({});
+  
+  constructor(
+    private fb: FormBuilder
+  ){}
 
+  ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario(){
+    this.fbGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  RecuperarClave(){
+    if(this.fbGroup.invalid){
+      alert('Debe ingresar los datos requeridos');
+    }else{
+      alert('Recuperando clave');
+    }
+  }
+
+  get obtenerFormGroup(){
+    return this.fbGroup.controls;
+  }
 }
