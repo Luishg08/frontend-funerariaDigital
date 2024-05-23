65966c5443cd76efdf3cbabbf7a92d4fe6c523d3
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-05-23 15:17:11 -0500

    Creacion de interfaces de inicio sesion, registro usuario, recuperar clave

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IdentificacionUsuarioComponent } from './identificacion-usuario/identificacion-usuario.component';
+import { IdentificacionTwofaComponent } from './identificacion-twofa/identificacion-twofa.component';
+import { CrearUsuarioComponent } from './usuario/crear-usuario/crear-usuario.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
 const routes: Routes = [
   {
     path:'identificar-usuario',
     component: IdentificacionUsuarioComponent
+  },
+  {
+    path: 'identificacion-twofa',
+    component: IdentificacionTwofaComponent
+  },
+  {
+    path: 'crear-usuario',
+    component: CrearUsuarioComponent
+  },
+  {
+    path: 'recuperar-clave',
+    component: RecuperarClaveComponent
   }
 ];
 
