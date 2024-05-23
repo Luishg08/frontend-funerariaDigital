fbef85fa2abe05805389bd763b864ff583c96af3
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-05-23 17:14:56 -0500

    Se creo la validacion de los datosn en el form de inicio de sesion

--- src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
+++ src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-identificacion-twofa',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './identificacion-twofa.component.html',
   styleUrl: './identificacion-twofa.component.css'
 })
--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-usuario',
@@ -7,8 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './identificacion-usuario.component.css'
 })
 export class IdentificacionUsuarioComponent {
-  constructor() { }
+  fGroup: FormGroup=new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario(){
+    this.fGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]]
+    });
+  }
+
+  IdentificarUsuario(){
+    if(this.fGroup.invalid){
+      alert('Debe ingresar los datos requeridos');
+    }else{
+      alert('Usuario identificado');
+    }
+    
+  }
+
+  get obtenerFormGroup(){
+    return this.fGroup.controls;
   }
 }
