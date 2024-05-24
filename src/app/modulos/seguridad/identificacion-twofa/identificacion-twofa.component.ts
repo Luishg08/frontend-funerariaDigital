f43e23af04dbd7fbfbc42786b3ea16064f9473b0
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-05-23 19:04:47 -0500

    Se creo validacion de 2fa

--- src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
+++ src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-twofa',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './identificacion-twofa.component.css'
 })
 export class IdentificacionTwofaComponent {
+  fbGroup: FormGroup=new FormGroup({});
+  
+  constructor(
+    private fb: FormBuilder
+  ){}
 
+  ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario(){
+    this.fbGroup = this.fb.group({
+      Codigo2fa: ['', [Validators.required]]
+    });
+  }
+
+  ConfirmarCodigo(){
+    if(this.fbGroup.invalid){
+      alert('Ingrese el codigo de verificacion');
+    }else{
+      alert('Confirmando codigo');
+    }
+  }
+
+  get obtenerFormGroup(){
+    return this.fbGroup.controls;
+  }
 }
