e3c247975bbc7d06722af1ef540b083d59c6c093
sophiCardona <sophia.cardona39931@ucaldas.edu.co>
2024-05-23 20:26:58 -0500

    Creacion del componente menu lateral y cambios en la carpeta assets

---
+++ src/assets/js/funcionesM.js
@@ -0,0 +1,11 @@
+function IniciarMenuLateral(){
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sr-only');
+        var instances = M.Sidenav.init(elems, {
+
+        });
+    })
+}
+
+IniciarMenuLateral();
+alert("hola, desde funcionesM.js")
