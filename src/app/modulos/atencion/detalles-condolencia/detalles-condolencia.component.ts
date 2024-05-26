32b329c97ba4f201fe80632452dc9ce2ed11263a
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-05-26 17:28:42 -0500

    Se creó una interface de detalles de condolencia

--- src/app/modulos/atencion/detalles-condolencia/detalles-condolencia.component.ts
+++ src/app/modulos/atencion/detalles-condolencia/detalles-condolencia.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-detalles-condolencia',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './detalles-condolencia.component.html',
   styleUrl: './detalles-condolencia.component.css'
 })
